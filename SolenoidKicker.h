f1d1900ee7c634314adb33fe6cfb6b446679693d
horsepork <kennydhubbell@gmail.com>
2023-08-31 16:25:04 -0500

    Initial commit

---
+++ SolenoidKicker.h
@@ -0,0 +1,136 @@
+#ifndef SOLENOIDKICKER_H
+#define SOLENOIDKICKER_H
+
+#include "Arduino.h"
+#include "BooleanInputBase.h"
+#include "Shift_Input.h"
+#include "Shift_Output.h"
+
+// forceKick() not implemented, not sure if needed
+
+class SolenoidKicker{
+    public:
+        SolenoidKicker(uint8_t _kickPin, BooleanInputBase* _kickerSensor){
+            kickerOutputType = DIGITAL_WRITE_OUTPUT;
+            kickerSensor = _kickerSensor;
+            kickerSensor = _kickerSensor;
+            kickerStatus = NOT_KICKING;
+        }
+
+        SolenoidKicker(Shift_Output* _shiftOutput, uint8_t _shiftOutputIndex, BooleanInputBase* _kickerSensor){
+            shiftOutput = _shiftOutput;
+            shiftOutputIndex = _shiftOutputIndex;
+            kickerOutputType = SHIFT_REGISTER_OUTPUT;
+            kickerSensor = _kickerSensor;
+            kickerStatus = NOT_KICKING;
+        }
+
+        void update(){
+            switch(kickerStatus){
+                case NOT_KICKING:
+                    if(kickState == HIGH){
+                        setKickOutputState(HIGH);
+                        kickTimer = millis();
+                        kickerStatus = KICKING;
+                    }
+                    break;
+                case KICKING:
+                    if(!isEngaged() || kickState == LOW){
+                        kickerStatus = NOT_KICKING;
+                        setKickOutputState(LOW);
+                    }
+                    else if(millis() - kickTimer > kickLength){
+                        kickerStatus = IN_BETWEEN_KICKS;
+                        kickTimer = millis();
+                    }
+                    break;
+                case IN_BETWEEN_KICKS:
+                    if(kickState == LOW){
+                        kickerStatus = NOT_KICKING;
+                        setKickOutputState(LOW); // for safety, but otherwise unnecessary
+                    }
+                    else if(millis() - kickTimer > timeBetweenKicks){
+                        kickerStatus = KICKING;
+                        setKickOutputState(HIGH);
+                        kickTimer = millis();
+                    }
+                    break;
+            }
+        }
+
+        void setKickState(bool newKickState){
+            kickState = newKickState;
+        }
+
+        void kick(){
+            setKickState(true);
+        }
+
+        void unkick(){
+            setKickState(true);
+        }
+
+        void invertOutput(bool _outputInverted){ // if we end up running the output through a relay, say
+            outputInverted = _outputInverted;
+        }
+
+        void setKickLength(uint16_t newKickLength){
+            kickLength = newKickLength;
+        }
+
+        void setTimeBetweenKicks(uint32_t _timeBetweenKicks){
+            timeBetweenKicks = _timeBetweenKicks;
+        }
+
+        bool isEngaged(){
+            return kickerSensor->read();
+        }
+
+        void forceKick(){
+            
+        }
+    
+    private:
+        bool kickState = LOW;
+        bool outputInverted = false;
+
+        uint32_t kickTimer;
+        uint16_t kickLength = 75;
+        uint32_t timeBetweenKicks = 2000;
+
+        uint8_t kickPin;
+
+        Shift_Output* shiftOutput = nullptr;
+        uint8_t shiftOutputIndex;
+        
+        BooleanInputBase* kickerSensor;
+
+        enum {
+            DIGITAL_WRITE_OUTPUT,
+            SHIFT_REGISTER_OUTPUT
+        } kickerOutputType;
+
+        enum {
+            NOT_KICKING,
+            KICKING,
+            IN_BETWEEN_KICKS
+        } kickerStatus;
+
+        void setKickOutputState(bool newState){
+            if(outputInverted){
+                newState = !newState;
+            }
+            switch(kickerOutputType){
+                case DIGITAL_WRITE_OUTPUT:
+                    digitalWrite(kickPin, newState);
+                    break;
+                case SHIFT_REGISTER_OUTPUT:
+                    shiftOutput->write(shiftOutputIndex, newState);
+                    break;
+            }
+        }
+
+
+};
+
+#endif
---
+++ SolenoidKicker_Example.ino
@@ -0,0 +1,32 @@
+#include "Arduino.h"
+#include "SolenoidKicker.h"
+#include "Shift_Input.h"
+#include "Shift_Output.h"
+#include "Debounced_DigitalRead.h"
+
+#define digitalOutputPin 4
+#define kickerSensorPin 5
+Debounced_DigitalRead kickerSensor(kickerSensorPin, INPUT_PULLUP);
+
+Debounced_DigitalRead otherKickerSensor(6, INPUT_PULLUP);
+
+
+SolenoidKicker digitalOutputKicker(digitalOutputPin, kickerSensor);
+
+Shift_Output shiftOut(2, 3, 4); // made up clock, load, and latch pins
+#define SHIFT_OUT_KICK_INDEX 0
+SolenoidKicker shiftOutputKicker(&shiftOut, SHIFT_OUT_KICK_INDEX, otherKickerSensor);
+
+void setup(){
+    shiftOut.begin();
+    kickerSensor.begin();
+    otherKickerSensor.begin();
+}
+
+void loop(){
+    kickerSensor.update();
+    otherKickerSensor.update();
+    digitalOutputKicker.update(); // call in loop
+    digitalOutputKicker.setKickState(HIGH); // will cause a periodic kick until sensor is disengaged
+    digitalOutputKicker.setKickState(LOW); // halt kicking
+}
